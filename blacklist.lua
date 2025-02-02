27a772905fee26a0ad3d43b2d6ecf1e6400bfdea
bmlkamanhah17 <developer11hh@gmail.com>
2025-02-02 22:44:55 +0300

    Create blacklist.lua

---
+++ blacklist.lua
@@ -0,0 +1,6 @@
+return {
+    {UserId = 7837080873, HWID = "wpwp19zv", IP = "192.168.1.1"},
+    {UserId = 7639808387, HWID = "DEF456", IP = "NG"},
+    {UserId = 5416068413, HWID = "NG", IP = "203.0.113.5"},
+    {UserId = 2929927986, HWID = "XYZ789", IP = "NG"}
+}
